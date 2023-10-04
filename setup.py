4e4ae8a0fbdf38ee678cc4185a3352a9048c32f2
Daniel Tasada <daniel.tasada@icloud.com>
2023-10-04 09:08:23 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    include_package_data=True,
+    name="Ohio Trail",
+    version="0.1",
+    description="ohio lmao",
+    author="Daniel Tasada, kleinbroodje, LMCuber",
+    packages=find_packages(),
+    install_requires=["pygame-ce"],
+)
