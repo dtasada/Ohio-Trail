27ed90d13ea95b1dab769223d21e87eebeed70e9
Daniel Tasada <daniel.tasada@icloud.com>
2023-10-05 19:12:41 +0200

    ttt in progress

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from types import coroutine
 from settings import *
 from character import *
 
@@ -29,6 +30,8 @@ class TicTacToe:
         self.pos = [1, 1]
         self.cross_x = self.size/3 + self.xo
         self.cross_y = self.size/3 + self.yo
+        self.crossed_positions = []
+        self.circled_positions = []
         self.lines = [
             [(0, self.size/3),     (self.size, self.size/3)],
             [(0, 2/3 * self.size), (self.size, 2/3 * self.size)],
@@ -46,11 +49,15 @@ class TicTacToe:
             self.pos[0] += 1
         if event.key in (pygame.K_LEFT, pygame.K_a) and self.pos[0] != 0:
             self.pos[0] -= 1
+        if event.key in (pygame.K_RETURN, pygame.K_SPACE) and not self.pos in ([crossed_position[0] for crossed_position in self.crossed_positions], [circled_position[0] for circled_position in self.circled_positions]):
+            self.crossed_positions.append((self.pos, self.cross_x, self.cross_y))
 
     def update(self):
         self.cross_x = self.pos[0] * self.size/3 + 1.07 * self.xo
         self.cross_y = self.pos[1] * self.size/3 + 1.14 * self.yo
         self.cross, self.cross_rect = writ('x', (self.cross_x, self.cross_y), 40)
+        for cross in self.crossed_positions:
+            writ('x', (cross[1], cross[2]), 40)
         REN.blit(self.cross, self.cross_rect)
         for line in self.lines:
             draw_line(REN, WHITE, (line[0][0] + self.xo, line[0][1] + self.yo), (line[1][0] + self.xo, line[1][1] + self.yo))
@@ -198,7 +205,7 @@ all_entries = []
 name_entry = RetroEntry("Hello traveler, what is your name?", (0, 0), command=ask_background)
 all_entries.append(name_entry)
 
-update_objects = []
+update_objects = [ ttt ]
 
 def main():
     running = __name__ == "__main__"
