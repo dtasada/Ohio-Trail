f7f4b5382e1d7d59785b7bd300e641dbc26ff5cb
Daniel Tasada <daniel.tasada@icloud.com>
2023-10-05 12:42:11 +0200

    ttt

--- src/main.py
+++ src/main.py
@@ -24,6 +24,7 @@ class TicTacToe:
         self.size = 300
         self.xo = WIDTH/2 - self.size/2
         self.yo = HEIGHT/2 - self.size/2
+        self.pos = [1, 1]
         self.cross_x = self.size/3 + self.xo
         self.cross_y = self.size/3 + self.yo
         self.lines = [
@@ -34,8 +35,21 @@ class TicTacToe:
             [(2/3 * self.size, 0), (2/3 * self.size, self.size)],
         ]
 
+    def process_event(self, event):
+        if event.key in (pygame.K_DOWN, pygame.K_s) and self.pos[1] != 2:
+            self.pos[1] += 1
+        if event.key in (pygame.K_UP, pygame.K_w) and self.pos[1] != 0:
+            self.pos[1] -= 1
+        if event.key in (pygame.K_RIGHT, pygame.K_d) and self.pos[0] != 2:
+            self.pos[0] += 1
+        if event.key in (pygame.K_LEFT, pygame.K_a) and self.pos[0] != 0:
+            self.pos[0] -= 1
+
     def update(self):
-        self.cross, self.cross_rect = writ('x', (self.cross_x, self.cross_y))
+        print(self.pos)
+        self.cross_x = self.pos[0] * self.size/3 + self.xo
+        self.cross_y = self.pos[1] * self.size/3 + self.yo
+        self.cross, self.cross_rect = writ('x', (self.cross_x, self.cross_y), 40)
         REN.blit(self.cross, self.cross_rect)
         for line in self.lines:
             draw_line(REN, WHITE, (line[0][0] + self.xo, line[0][1] + self.yo), (line[1][0] + self.xo, line[1][1] + self.yo))
