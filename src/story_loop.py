5aa21381f4d1641afd0a76287c826faee108488e
Leo Bozkir <leo.bozkir@outlook.com>
2025-03-14 17:07:12 +0100

    kinda fixed texurreu bugs

--- src/story_loop.py
+++ src/story_loop.py
@@ -298,7 +298,6 @@ def explore_forest():
         player.complete(Completed.EXPLORED_FOREST)
 
 
-@checkpoint
 def talk_to_merchant():
     active_widgets.clear()
 
@@ -328,7 +327,7 @@ def merchant_selection():
             actions=[f"{item.name} (${item.price})" for item in shop_list] + ["Leave"],
             pos=(0, 60),
             command=buy_item,
-            images=[i.tex for i in shop_list],
+            images=[i.img for i in shop_list],
             image_rects=[pygame.Rect(550, 190, 150, 150) for i in shop_list],
         )
     )
